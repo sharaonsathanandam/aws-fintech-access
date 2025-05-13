77a4ef34ad5f20ab9b207d427c9122f5d128ffdd
sharaonabishek <sharaonm@bbinsight.com>
2025-05-13 11:46:44 -0500

    Grant access for fin_analyst1 on datatset2

--- scripts/revoke_access.py
+++ scripts/revoke_access.py
@@ -13,7 +13,6 @@ except subprocess.CalledProcessError:
     sys.exit(0)
 
 data = yaml.safe_load(content)
-data["is_access_request"] = False
 
 if not data.get("is_access_request", False):
     print(f"Skipping: {yaml_path} was not an access request file.")
@@ -25,6 +24,7 @@ account_id = subprocess.check_output([
 ]).decode("utf-8").strip()
 # data["principal_arn"] = f"arn:aws:iam::{account_id}:role/AWSReservedSSO_FinanceAnalysts_b67570c300321d27"
 
+data["is_access_request"] = False
 with open("pipeline-config/terraform.tfvars.json", "w") as f:
     json.dump(data, f, indent=2)
 
